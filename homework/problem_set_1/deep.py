3a5dfaf2563718f94d999ee8972e2de1836acfb9
elbergbt <ldegald@gmail.com>
2025-09-06 19:51:45 +0700

    task 1 from problem set 1

---
+++ homework/problem_set_1/deep.py
@@ -0,0 +1,14 @@
+#need practise
+n = input("What is the answer to the Great Question of Life, the Universe and Everything? ")
+n = n.strip()
+
+if n =="42":
+    print("yes")
+elif n == "forty-two":
+    print("yes")
+elif n == "forty two":
+    print("yes")
+elif n == "Forty Two":
+    print("yes")
+else:
+    print("no")

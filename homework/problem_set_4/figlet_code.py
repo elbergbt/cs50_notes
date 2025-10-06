423944eb30a23df8b1edde1d50ba155bccfdc6bd
elbergbt <ldegald@gmail.com>
2025-10-06 21:06:20 +0700

    fix condition

--- homework/problem_set_4/figlet_code.py
+++ homework/problem_set_4/figlet_code.py
@@ -15,7 +15,7 @@ if len(sys.argv) == 1:
     fig.setFont(font=random_font)
     print(f"Output: {fig.renderText(user_input)}")
 elif len(sys.argv) == 3:
-    if (sys.argv[1] != "-f" and sys.argv[1] != "--font") or sys.argv[2] not in font_names:
+    if sys.argv[1] not in ("-f", "--font") or sys.argv[2] not in font_names:
         sys.exit("Invalid usage")
     else:
         fig.setFont(font=sys.argv[2])
